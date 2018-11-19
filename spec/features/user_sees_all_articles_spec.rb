bb98a1fb308109b3f760a2724870090e421cfa5a
manojpanta <manojpanta95@gmail.com>
2018-11-19 15:04:32 -0700

    passing test for links of articles

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -9,10 +9,19 @@ describe 'user sees all articles' do
       visit '/articles'
 
       expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
       expect(page).to have_content(article_2.title)
-      expect(page).to have_content(article_2.body)
+    end
+  end
 
+  describe 'they visit /articles' do
+    it 'displays all the article links' do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+      visit '/articles'
+
+      expect(page).to have_link(article_1.title)
+      expect(page).to have_link(article_2.title)
     end
   end
 end
